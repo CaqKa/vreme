b5876dbf5ae84dc8e99502062fa49776eb883238
CaqKa <danilo.majhenic@gmail.com>
2021-02-16 15:17:47 +0100

    Izpisujem temperaturo
    Izpiše temperaturo

--- ARSO_API.py
+++ ARSO_API.py
@@ -14,5 +14,13 @@ def meteoarsogovsi(url):
     r = requests.get("http://meteo.arso.gov.si"+url)
 #     data = r.json()
 #     vremenskaNapoved = json.loads(data)
-#     print(json.dumps(vremenskaNapoved))
     return r.content
+
+
+# Naloži xml preko requests
+def prikaziNapoved():
+    url = "/uploads/probase/www/fproduct/text/sl//forecast_SI_PODRAVSKA_int3h.xml"
+#    napoved = json.loads(meteoarsogovsi(url))
+#    print(json.dumps(napoved, indent=1))
+    napoved = meteoarsogovsi(url)
+    return napoved
--- vreme.py
+++ vreme.py
@@ -1,22 +1,18 @@
 # Vreme izris
 # TODO Izrišem temperature iz dvodnevne arso napovedi za podravsko regijo
-
 # TODO preberem podatke iz 5-dnevne napovedi ARSO: http://meteo.arso.gov.si/uploads/probase/www/fproduct/text/sl/forecast_SI_PODRAVSKA_latest.xml
 # XML iz katerega jemljem podatke je: http://meteo.arso.gov.si/uploads/probase/www/fproduct/text/sl//forecast_SI_PODRAVSKA_int3h.xml
 
-from ARSO_API import meteoarsogovsi as arso
+import ARSO_API as arso
 import json
 from lxml import etree
+import xmltodict, json
 
-def prikaziNapoved():
-    url = "uploads/probase/www/fproduct/text/sl//forecast_SI_PODRAVSKA_int3h.xml"
-#    napoved = json.loads(arso(url))
-#    print(json.dumps(napoved, indent=1))
-#    napoved = arso(url)
-#    return napoved
 
-prikaziNapoved()
-url="http://meteo.arso.gov.si/uploads/probase/www/fproduct/text/sl//forecast_SI_PODRAVSKA_int3h.xml"
-tree = etree.parse(url)
-convert(tree)
-print(tree)
+
+xmlNapoved = arso.prikaziNapoved()
+obj = xmltodict.parse(xmlNapoved)
+napovedneTocke = obj['data']['metData']
+print(json.dumps(obj['data']['metData']))
+for tocka in napovedneTocke:
+    print(tocka['t'])
